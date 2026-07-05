b34f7e81faefb902a45dba2be70b024a55ad725d
jl-cvyrj <karolina.pavlouvkaya9072@gmail.com>
2026-07-05 17:46:26 +0300

    add db

---
+++ src/main/java/com/innowise/paymentservice/PaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.paymentservice;
+
+import io.mongock.runner.springboot.EnableMongock;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableMongock
+@SpringBootApplication
+public class PaymentServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceApplication.class, args);
+    }
+}
